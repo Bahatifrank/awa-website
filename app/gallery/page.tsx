a7fec9d5838c2f6766c75b575f3c45073185ec82
Bahatifrank <bahatifrank908@gmail.com>
2026-09-14 17:29:06 +0300

    Update project changes

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -24,7 +24,7 @@ const photos = [
   { src: "/gallery/pic20.jpg", alt: "Adolescents engaged in an AWA resilience-building exercise" },
   { src: "/gallery/pic21.jpg", alt: "AWA mentorship session photo from a school visit" },
   { src: "/gallery/pic22.jpg", alt: "AWA team facilitating a values-based education session" },
-  { src: "/gallery/q2.jpeg", alt: "the mentor spending time with the senior football team" },
+  { src: "/gallery/q2.jpg", alt: "the mentor spending time with the senior football team" },
   { src: "/gallery/pic23.jpg", alt: "Adolescents participating in an AWA prevention program activity" },
   { src: "/gallery/pic24.jpg", alt: "AWA gallery photo from a community wellness event" },
   { src: "/gallery/pic25.jpg", alt: "AWA mentorship session with secondary school students" },
@@ -36,7 +36,7 @@ const photos = [
   { src: "/gallery/D5.jpeg", alt: "AWA counselling session with an adolescent participant" },
   { src: "/gallery/D4.jpeg", alt: "AWA team building activity with program participants" },
   { src: "/gallery/D1.jpeg", alt: "Adolescents taking part in an AWA workshop activity" },
-  { src: "/gallery/q1.jpeg", alt: "having fun during community work with a football team" },
+  { src: "/gallery/q1.jpg", alt: "having fun during community work with a football team" },
 
 ];
 
